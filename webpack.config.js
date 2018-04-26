4dba51231ba6e1c9bcf87e07841e604af623534f
zhengyangca <billyoung119@gmail.com>
2018-04-25 23:34:16 -0230

    assetsPublicPath: './'

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/dist/'),
         filename: '[name].js',
+        assetsPublicPath: './'，
         publicPath: '/'
     },
     plugins: [
